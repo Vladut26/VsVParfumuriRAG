24f10e647795486def0339f8e70ef4b925876954
Vladut Vasile <stefanvlad262002@gmail.com>
2026-06-23 00:44:35 +0300

    updated allowed origins

--- server/src/main/java/com/vsv/config/SecurityConfig.java
+++ server/src/main/java/com/vsv/config/SecurityConfig.java
@@ -90,7 +90,8 @@ public class SecurityConfig {
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
                 "http://localhost:3000",
-                "http://localhost:4173"
+                "http://localhost:4173",
+                "vsvparfumurirag-production-6e9e.up.railway.app"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
